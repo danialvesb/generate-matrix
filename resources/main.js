0ca7a336b562c8efdf30f1742b480566cf1a3686
Daniel Alves Bezerra <47247720+danialvesb@users.noreply.github.com>
2020-11-06 19:27:40 -0300

    first

---
+++ resources/main.js
@@ -0,0 +1,67 @@
+const vm = new Vue({
+    el: '#app',
+    methods: {
+        getRandomPair() {
+            let value = parseInt(Math.random() * 100)
+            if ((value % 2) == 0 && value != 0) {
+                return value;
+            }
+            return this.getRandomPair();
+        },
+        getRandomOdd() {
+            let value = parseInt(Math.random() * 100)
+            if ((value % 2) == 1 && value != 0) {
+                return value;
+            }
+            return this.getRandomOdd();
+        },
+        generateMatrix(typeNumbers) {
+            let matrix = [
+                [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                ],
+                [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                ],
+                [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+
+                ],
+                [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+
+                ],
+                [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                ],
+            ];
+
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    matrix[i][j] = typeNumbers === 2 ? this.getRandomPair() : this.getRandomOdd();
+                }
+            }
+            return matrix;
+        },
+    },
+});
